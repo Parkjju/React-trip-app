4d9652d76971f2dfae6d2ffe32667e1e430174a7
Parkjju <rudwns3927@gmail.com>
2021-09-25 20:21:11 +0900

    [feat] 목 서버 API 테스트 코드 작성

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ axios
   .catch(Error => {
     console.log(Error);
   });
+
+axios
+  .get('https://5b454fe7-e619-4f04-88e2-fd600d083186.mock.pstmn.io/travel/1')
+  .then(Response => {
+    console.log(Response.data);
+  })
+  .catch(Error => {
+    console.log(Error);
+  });
