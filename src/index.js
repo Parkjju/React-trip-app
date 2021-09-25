a785616a4175c510cae46e3987fd872c6af4253c
Parkjju <rudwns3927@gmail.com>
2021-09-25 20:16:59 +0900

    [chore] 프로젝트 디렉토리 구조 수정

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+axios
+  .get('https://5b454fe7-e619-4f04-88e2-fd600d083186.mock.pstmn.io/location')
+  .then(Response => {
+    console.log(Response.data);
+  })
+  .catch(Error => {
+    console.log(Error);
+  });
